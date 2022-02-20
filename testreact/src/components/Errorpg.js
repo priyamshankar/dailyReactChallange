dbc12dcbd1af26a834cb70847d2bd703050785c7
priyam <priyamshankar.5@gmail.com>
2022-02-21 01:50:01 +0530

    making the login

--- testreact/src/components/Errorpg.js
+++ testreact/src/components/Errorpg.js
@@ -1,12 +1,45 @@
 import React from 'react'
 import { NavLink as Link } from 'react-router-dom';
+import './css/errorpg.css';
 
 const Errorpg = () => {
   return (
     <>
-    This is an Error page.
-    <Link to='/'>go to Home</Link>
+  < Link to="" target="_blank">
+  <header className="top-header">
+</header>
 
+<div>
+  <div className="starsec"></div>
+  <div className="starthird"></div>
+  <div className="starfourth"></div>
+  <div className="starfifth"></div>
+</div>
+
+<div className="lamp__wrap">
+  <div className="lamp">
+    <div className="cable"></div>
+    <div className="cover"></div>
+    <div className="in-cover">
+      <div className="bulb"></div>
+    </div>
+    <div className="light"></div>
+  </div>
+</div>
+<section className="error">
+
+  <div className="error__content">
+    <div className="error__message message">
+      <h1 className="message__title">Page Not Found</h1>
+      <p className="message__text">We're sorry, the page you were looking for isn't found here. The link you followed may either be broken or no longer exists. Please try again, or take a look at our.</p>
+    </div>
+    <div className="error__nav e-nav">
+      < Link to="" target="_blanck" className="e-nav__link"></Link>
+    </div>
+  </div>
+</section>
+
+  </Link>
     </>
   )
 }
--- testreact/src/components/Login.js
+++ testreact/src/components/Login.js
@@ -1,13 +1,49 @@
-import React from 'react'
-import { NavLink as Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { NavLink as Link } from "react-router-dom";
+import "./css/login.css";
 
-const login = () => {
+const Login = () => {
+  let [login, setlogin] = useState({
+    userId: "",
+    password: "",
+  });
+  const getData = (event) => {
+    let name=event.target.name;
+    let value=event.target.value;
+    setlogin({...login, [name]:value})  
+  };
+
   return (
     <>
-    <div>this is a login page</div>
-    <Link to='/tub'>go to logout</Link>
-    </>
-  )
-}
+      <div>this is a login page</div>
+      <Link to="/tub">go to logout</Link>
 
-export default login
+      <div className="login">
+        <div className="form">
+          <form className="login-form">
+            <span className="material-icons">lock</span>
+            <input
+              type="text"
+              placeholder="email"
+              required
+              name="userId"
+              value={login.userId}
+              onChange={getData}
+            />
+            <input
+              type="password"
+              placeholder="password"
+              required
+              name="password"
+              value={login.password}
+              onChange={getData}
+            />
+            <button>login</button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
